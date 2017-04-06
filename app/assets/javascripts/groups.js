f15f48850108143c7694b75143736c1f6ee332e1
mwei2509 <m2dl89@gmail.com>
2017-04-05 20:00:55 -0400

    fixes

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -6,7 +6,7 @@ function submitNewMessage(){
   $('textarea#message_body').keydown(function(event) {
     if (event.keyCode == 13) {
         $('input[type=submit]').submit();
-        $('#message_body').val(" ")
+        $('#message_body').val("")
         return false;
      }
   });
